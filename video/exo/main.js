586330cf26d1a47dbc06c58dfb9b8736802be40b
Elisa BOURG <elisa@macbook-pro-de-elisa.home>
2023-02-15 13:46:13 +0100

    feat: add duration and current time'

--- video/exo/main.js
+++ video/exo/main.js
@@ -5,6 +5,9 @@ video.removeAttribute("controls");
 controls.style.visibility = "visible";
 let play = document.getElementById("play");
 let mute = document.getElementById("mute");
+let rewind = document.getElementById("rewind");
+let current = document.getElementById("current");
+let duration = document.getElementById("duration");
 
 /* Video controls */
 
@@ -40,7 +43,7 @@ function playPauseOnVideo() {
    }
 }
 
-
+/* put the volume of the video */
 function unmuteVideo() {
   if (video.muted) {
      video.muted = false;
@@ -51,5 +54,22 @@ function unmuteVideo() {
     mute.innerText = "UnMute";
   }
 }
+/* put the video at the start function */
+function rewindVideo() {
+  video.pause();
+  video.currentTime = 0;
+}
+/* put the duration and the time of the video */
+function getTimeVideo() {
+   let currentTime = video.currentTime;
+   current.innerText = Math.floor(currentTime)/100;
+   let durationVideo = video.duration;
+   duration.innerText = Math.floor(durationVideo) / 100;
+
+}
+
 play.addEventListener('click', playPauseOnVideo);
 mute.addEventListener('click', unmuteVideo);
+rewind.addEventListener('click', rewindVideo);
+video.addEventListener('timeupdate', getTimeVideo);
+
